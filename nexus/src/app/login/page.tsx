543fd5eba712e86a14c17a7695bc7b220a23dc93
Gabriel <gabrielferreiranunes@live.com>
2025-07-17 13:38:06 -0300

    Estilizando tela de login

--- nexus/src/app/login/page.tsx
+++ nexus/src/app/login/page.tsx
@@ -1,28 +1,32 @@
-// src/app/login/page.tsx
 
-// Esta é a definição do nosso componente de página de login.
-// Tudo que estiver dentro do `return` será renderizado na tela.
 export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <div className="max-w-md w-full  p-8 rounded-lg shadow-md">
-            <h1 className="text-2xl font-bold mb-6 text-center">Nexus Login</h1>
-            <form>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
 
-                <div>
-                    <label htmlFor="Email"> Email </label>
-                    <input id="email" name="email" type="email"/>
-                </div>
+            <div className="max-w-md w-full bg-sky-500/50 text-black p-8 rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold mb-6 text-center">Nexus Login</h1>
+                <form className="space-y-6">
 
-                <div>   
-                    <label htmlFor="password"> Senha </label>
-                    <input id="password" name="password" type="password"/>    
-                </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium"> Email </label>
+                        <input id="email" type="email" required className="peer mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 hover:border-blue-700 shadow-sm" placeholder="voce@email.com" />
+                        </div>
 
-            </form>
+                    <div>
+                        <label
+                            htmlFor="password" className="block text-sm font-medium"> Senha </label>
+                        <input id="password" type="password" required className="peer mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 hover:border-blue-700 shadow-sm"/>
+                    </div>
 
-        </div>
-        
-    </main>
-  );
+                    <div>
+                        <button type="submit" className="mt-6 px-4 py-2 bg-black text-white rounded border-gray-300 hover:bg-blue-700 shadow-sm">
+                            Entrar
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+
+        </main>
+    );
 }
